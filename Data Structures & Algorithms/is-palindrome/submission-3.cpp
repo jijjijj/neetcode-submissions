fe737a2d75df145d9732491056aaa5b036624a63
jijjijj <realjijjijj@gmail.com>
2026-07-14 13:08:57 +0300

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0;
+        int r = s.size() - 1;
+
+        while (l < r) {
+            if (!std::isalnum(s[l])) ++l;
+            if (!std::isalnum(s[r])) --r;
+
+            if (std::tolower(s[l++]) != std::tolower(s[r--])) return false;
+        }
+
+        return true;
+    }
+};
