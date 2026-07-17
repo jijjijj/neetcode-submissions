774a936fa85d6bf91a5a70d20d4b531e15982194
jijjijj <realjijjijj@gmail.com>
2026-07-17 23:07:35 +0300

    Add: merge-triplets-to-form-target - submission-3

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        std::vector<bool> found(target.size());
+
+        for (const auto& t : triplets) {
+            if (t[0] > target[0] ||
+                t[1] > target[1] ||
+                t[2] > target[2]) return false;
+            
+            for (int i = 0; i < t.size(); ++i) {
+                found[i] = found[i] || (t[i] == target[i]);
+            }
+        }
+
+        return found[0] && found[1] && found[2];
+    }
+};
