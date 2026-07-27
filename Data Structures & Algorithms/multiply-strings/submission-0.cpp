937923d2c4fa9a5539cb9e983915a677cb9f21a8
jijjijj <realjijjijj@gmail.com>
2026-07-27 23:49:34 +0300

    Add: multiply-strings - submission-0

---
+++ Data Structures & Algorithms/multiply-strings/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int len1 = num1.size();
+        int len2 = num2.size();
+
+        if (len1 < len2) {
+            std::swap(num1, num2);
+            std::swap(len1, len2);
+        }
+
+        std::string res(len1 + len2, '\0');
+
+        int carry = 0;
+        for (int i = 0; i < len1; ++i) {
+            carry = 0;
+            for (int j = 0; j < len2; ++j) {
+                const int n2 = num2[len2 - j - 1] - '0';
+                const int n1 = num1[len1 - i - 1] - '0';
+
+                char& cell = res[res.size() - i - j - 1];
+                int dig = n1 * n2 + carry + cell;
+
+                cell = (char)(dig % 10);
+                carry = dig / 10;
+            }
+        }
+
+        if (carry) res[0] = (char)carry;
+        else res = res.substr(1);
+
+        for (char& c : res) c += '0';
+
+        return res;
+    }
+};
