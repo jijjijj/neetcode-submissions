064a458c360e9688e3d1c7e228a11ec960e0d498
jijjijj <realjijjijj@gmail.com>
2026-07-30 00:10:36 +0300

    Add: non-cyclical-number - submission-4

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-4.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int slow = 0;
+        int fast = n;
+
+        int power = 1;
+        int len = 0;
+
+        while (slow != fast) {
+            if (power == len) {
+                slow = fast;
+                power <<= 1;
+                len = 0;
+            }
+
+            ++len;
+            fast = next(fast);
+        }
+
+        return slow == 1;
+    }
+
+private:
+    int next(int n) {
+        int power = 0;
+
+        while (n) {
+            power += (n % 10) * (n % 10);
+            n /= 10;
+        }
+
+        return power;
+    }
+};
