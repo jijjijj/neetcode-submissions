cfe93f1520f77518c9ddd2818fa2ca1a92d769d9
jijjijj <realjijjijj@gmail.com>
2026-08-02 15:40:46 +0300

    Add: interleaving-string - submission-5

---
+++ Data Structures & Algorithms/interleaving-string/submission-5.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        // dp[i][j] = is it possible to form s3[:i+j] 
+        // by interleaving s1[:i] and s2[:j]
+        const int len1 = s1.size();
+        const int len2 = s2.size();
+
+        if (s3.size() != len1 + len2) return false;
+
+        std::vector<int> dp(len2 + 1);
+
+        for (int i = len1; i >= 0; --i) {
+            for (int j = len2; j >= 0; --j) {
+                if (i == len1 && j == len2) { dp[j] = true; continue; }
+                bool cur = false;
+                if (i < len1 && s1[i] == s3[i + j] && dp[j]) cur = true;
+                if (j < len2 && s2[j] == s3[i + j] && dp[j + 1]) cur = true;
+                dp[j] = cur;
+            }
+        }
+
+        return dp.front();
+    }
+};
