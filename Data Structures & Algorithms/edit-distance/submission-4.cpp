80d134590b2bd9ff94266693fb7abd71dd0d5715
jijjijj <realjijjijj@gmail.com>
2026-08-02 23:04:21 +0300

    Add: edit-distance - submission-4

---
+++ Data Structures & Algorithms/edit-distance/submission-4.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        const int len1 = word1.size();
+        const int len2 = word2.size();
+
+        std::vector<int> dp(len2 + 1, 0);
+
+        for (int j = 0; j <= len2; ++j) dp[j] = len2 - j;
+
+        for (int i = len1 - 1; i >= 0; --i) {
+            dp[len2] = len1 - i - 1;
+            int prev = dp.back();
+            for (int j = len2 - 1; j >= 0; --j) {
+                int temp = dp[j];
+                if (word1[i] == word2[j]) {
+                    dp[j] = prev;
+                } else {
+                    dp[j] = 1 + std::min({
+                        dp[j + 1],
+                        dp[j],
+                        prev
+                    });
+                }
+                prev = temp;
+            }
+        }
+
+        return dp[0];
+    }
+};
