87b761bebea67e7a5a6635f31fa8a8bd71d5a3cd
jijjijj <realjijjijj@gmail.com>
2026-08-06 23:21:48 +0300

    Add: permutation-string - submission-9

---
+++ Data Structures & Algorithms/permutation-string/submission-9.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        const int n1 = s1.size();
+        const int n2 = s2.size();
+
+        std::unordered_map<char, int> freq1;
+        std::unordered_map<char, int> freq2;
+
+        for (char c = 'a'; c <= 'z'; ++c) {
+            freq1[c];
+            freq2[c];
+        }
+
+        for (const char c : s1) ++freq1[c];
+        for (int i = 0; i < n1 - 1; ++i) ++freq2[s2[i]];
+
+        int matches = 0;
+        for (const auto [c, cnt] : freq1) {
+            if (freq2[c] == cnt) ++matches;
+        }
+        
+        int l = 0;
+        for (int i = n1 - 1; i < n2; ++i) {
+            const char c = s2[i];
+
+            ++freq2[c];
+            if (freq2[c] == freq1[c]) ++matches;
+            else if (freq1[c] + 1 == freq2[c]) --matches;
+
+            if (matches == 26) return true;
+
+            const char c2 = s2[l];
+            --freq2[c2];
+            if (freq2[c2] == freq1[c2]) ++matches;
+            else if (freq1[c2] - 1 == freq2[c2]) --matches;
+
+            ++l;
+        }
+
+        return false;
+    }
+};
