42adc99c7846e2548e2b58ce4bd97dc699258428
jijjijj <realjijjijj@gmail.com>
2026-08-07 12:45:47 +0300

    Add: stone-game-ii - submission-1

---
+++ Data Structures & Algorithms/stone-game-ii/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        return dfs(piles, 0, 1, 1);
+    }
+
+private:
+    int dfs(const std::vector<int>& piles, int off, int alice, int m) {
+        const int n = piles.size();
+        if (off >= n) return 0;
+        
+        int max = alice ? 0 : 9999;
+        int sum = 0;
+        for (int x = 1; x <= std::min(2 * m, n - off); ++x) {
+            sum += piles[off + x - 1];
+
+            if (alice)
+            max = std::max(max, sum + dfs(piles, off + x, !alice, std::max(x, m)));
+            else
+            max = std::min(max, dfs(piles, off + x, !alice, std::max(x, m)));
+        }
+        return max;
+    }
+};
