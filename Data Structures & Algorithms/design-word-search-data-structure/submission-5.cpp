8d97e28b28d29a2cd12e564ae90433844a36dd71
jijjijj <realjijjijj@gmail.com>
2026-08-12 21:36:06 +0300

    Add: design-word-search-data-structure - submission-5

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.cpp
@@ -0,0 +1,61 @@
+class WordDictionary {
+public:
+    WordDictionary() {
+        
+    }
+    
+    void addWord(string word) {
+        Node* node = &root;
+
+        for (int i = 0; i < word.size(); ++i) {
+            const int idx = word[i] - 'a';
+
+            if (!node->children[idx]) {
+                node->children[idx] = new Node{};
+            }
+
+            node = node->children[idx];
+        }
+
+        node->ends = true;
+    }
+    
+    bool search(string word) {
+        return search_dfs(word, 0, root);
+    }
+
+private:
+    struct Node {
+        Node* children[26]{};
+        bool ends = false;
+
+        ~Node() {
+            for (Node* c : children) delete c;
+        }
+    };
+
+    bool search_dfs(const string& word, int i, const Node& node) {
+        if (i >= word.size()) return node.ends;
+
+        const char ch = word[i];
+
+        for (int j = i; j < word.size(); ++j) {
+            if (ch == '.') {
+                for (Node* child : node.children) {
+                    if (child && search_dfs(word, j + 1, *child))
+                        return true;
+                }
+
+                return false;
+            } else if (node.children[ch - 'a']) {
+                return search_dfs(word, j + 1, *node.children[ch - 'a']);
+            }
+
+            return false;
+        }
+
+        return false;
+    }
+
+    Node root{};
+};
