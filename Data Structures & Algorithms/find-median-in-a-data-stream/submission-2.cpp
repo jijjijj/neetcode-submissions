bea519dbf541f417c1295c86e51b73a81c9f3a60
jijjijj <realjijjijj@gmail.com>
2026-08-14 21:23:29 +0300

    Add: find-median-in-a-data-stream - submission-2

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-2.cpp
@@ -0,0 +1,38 @@
+class MedianFinder {
+public:
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        if (second.empty() || num >= second.top()) {
+            second.push(num);
+
+            if (second.size() > first.size() + 1) {
+                const int t = second.top();
+                second.pop();
+                first.push(t);
+            }
+        } else {
+            first.push(num);
+
+            if (first.size() > second.size()) {
+                const int t = first.top();
+                first.pop();
+                second.push(t);
+            }
+        }
+    }
+    
+    double findMedian() {
+        if ((first.size() + second.size()) & 1)
+            return second.top();
+
+        return (first.top() + second.top()) / 2.0;
+    }
+
+private:
+    std::priority_queue<double> first;
+    std::priority_queue<double, std::vector<double>,
+        std::greater<>> second;
+};
