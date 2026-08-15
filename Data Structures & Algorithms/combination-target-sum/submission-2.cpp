a70f0a73151cab4e45218c80529b2ded9607dd58
jijjijj <realjijjijj@gmail.com>
2026-08-15 13:31:41 +0300

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        std::ranges::sort(nums);
+
+        std::vector<std::vector<int>> res;
+        std::vector<int> cur;
+
+        dfs(res, nums, 0, target, cur);
+
+        return res;
+    }
+
+private:
+    void dfs(
+        std::vector<std::vector<int>>& res,
+        const vector<int>& nums, int i,
+        int left, std::vector<int>& cur) {
+        if (left < 0 || i >= nums.size()) return;
+        if (!left) {
+            res.emplace_back(cur);
+            return;
+        }
+
+        cur.emplace_back(nums[i]);
+        dfs(res, nums, i, left - nums[i], cur);
+        cur.pop_back();
+
+        dfs(res, nums, i + 1, left, cur);
+    }
+};
