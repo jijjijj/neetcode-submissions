dfffdaa9a504e1a60cf50e4708b40e94170af4b8
jijjijj <realjijjijj@gmail.com>
2026-08-16 00:24:01 +0300

    Add: count-number-of-islands - submission-4

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-4.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        const std::vector<std::pair<int, int>> dirs = {
+            { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 }
+        };
+
+        const int h = grid.size();
+        const int w = grid[0].size();
+
+        int count = 0;
+        for (int y = 0; y < h; ++y) {
+            for (int x = 0; x < w; ++x) {
+                if (grid[y][x] == '1') {
+                    ++count;
+
+                    std::queue<std::pair<int, int>> q;
+                    q.push({ x, y });
+
+                    while (!q.empty()) {
+                        const auto [cx, cy] = q.front();
+                        q.pop();
+
+                        grid[cy][cx] = '0';
+
+                        for (auto [dx, dy] : dirs) {
+                            const int nx = cx + dx;
+                            const int ny = cy + dy;
+
+                            if (nx < 0 || ny < 0 || nx >= w ||
+                                ny >= h || grid[ny][nx] != '1') continue;
+                            
+                            q.push({ nx, ny });
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+};
