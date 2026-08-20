8d78df12939e36f49296e5abe923d15a837a359e
jijjijj <realjijjijj@gmail.com>
2026-08-21 00:19:51 +0300

    Add: split-array-largest-sum - submission-6

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-6.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        const int cInf = 1'000'000 * 1000 + 1;
+        const int n = nums.size();
+        std::vector<int> dp(n + 1, cInf);
+        std::vector<int> dp2(n + 1, cInf);
+ 
+        for (int t = 1; t <= k; ++t) {
+            dp2[n] = 0;
+            for (int i = n - 1; i >= 0; --i) {
+                dp[i] = cInf;
+                int cur = 0;
+                for (int j = i; j <= n - t; ++j) {
+                    cur += nums[j];
+                    dp[i] = std::min(dp[i],
+                        std::max(cur, dp2[j + 1]));
+                }
+            }
+            std::swap(dp2, dp);
+        }
+
+        return dp2.front();
+    }
+};
