3b3cea84f4d201f740896ee05195077ab86acd3b
jijjijj <realjijjijj@gmail.com>
2026-08-22 19:48:32 +0300

    Add: single-threaded-cpu - submission-1

---
+++ Data Structures & Algorithms/single-threaded-cpu/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        std::vector<std::tuple<int, int, int>> t; // enqt, i, proct
+        for (int i = 0; i < tasks.size(); ++i) {
+            const auto& task = tasks[i];
+            t.emplace_back(std::tuple{
+                task[0], i, task[1] });
+        }
+        std::sort(t.begin(), t.end());
+
+        std::vector<int> res;
+        using val_t = std::pair<int, int>; // proct, i
+        std::priority_queue<val_t,
+            std::vector<val_t>, std::greater<>> q;
+
+        int time = 0;
+        int i = 0;
+        while (!q.empty() || i < t.size()) {
+            while (i < t.size() && std::get<0>(t[i]) <= time) {
+                const auto [enq, idx, proc] = t[i];
+                q.push({ proc, idx });
+                ++i;
+            }
+
+            if (q.empty()) {
+                time = std::get<0>(t[i]);
+            } else {
+                res.emplace_back(q.top().second);
+                time += q.top().first;
+                q.pop();
+            }
+        }
+        
+        return res;
+    }
+};
