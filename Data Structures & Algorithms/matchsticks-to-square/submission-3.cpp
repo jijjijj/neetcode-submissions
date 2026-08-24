0ba9693e163cd6aaa2705a3045e67342e14dbedf
jijjijj <realjijjijj@gmail.com>
2026-08-24 16:47:00 +0300

    Add: matchsticks-to-square - submission-3

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-3.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    Solution() : sides(4) {}
+
+    bool makesquare(vector<int>& nums) {
+        sides.clear();
+        sides.resize(4);
+        
+        std::ranges::sort(nums);
+        const int total = std::accumulate(
+            nums.begin(), nums.end(), 0);
+
+        if (total % 4) return false;
+
+        return dfs(total / 4, nums, 0);
+    }
+
+private:
+    bool dfs(int total, std::vector<int>& nums, int i) {
+        if (sides[0] == sides[1] && sides[2] == sides[3] &&
+            sides[0] == sides[2] && sides[0] == total) {
+            return true;
+        }
+
+        for (int j = 0; j < 4; ++j) {
+            if (sides[j] + nums[i] > total) continue;
+
+            sides[j] += nums[i];
+            if (dfs(total, nums, i + 1)) return true;
+            sides[j] -= nums[i];
+        }
+
+        return false;
+    }
+
+    std::vector<int> sides;
+};
