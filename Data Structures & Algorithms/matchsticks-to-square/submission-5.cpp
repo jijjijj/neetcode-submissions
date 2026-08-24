c654d41fe6ed630bcd1bbde572eb9b71cfa72528
jijjijj <realjijjijj@gmail.com>
2026-08-24 17:44:39 +0300

    Add: matchsticks-to-square - submission-5

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-5.cpp
@@ -0,0 +1,46 @@
+#include <ranges>
+
+class Solution {
+public:
+    Solution() : sides(4) {}
+
+    bool makesquare(vector<int>& nums) {
+        sides.clear();
+        sides.resize(4);
+
+        std::ranges::sort(nums | std::views::reverse);
+        const int total = std::accumulate(
+            nums.begin(), nums.end(), 0);
+
+        if (total % 4) return false;
+
+        return dfs(total / 4, nums, 0, 0);
+    }
+
+private:
+    bool dfs(int total, std::vector<int>& nums, int i, uint64_t mask) {
+        if (sides[0] == sides[1] && sides[2] == sides[3] &&
+            sides[0] == sides[2] && sides[0] == total) {
+            return true;
+        }
+
+        // if (dp.contains(mask)) return false;
+
+        for (int j = 0; j < 4; ++j) {
+            if (sides[j] + nums[i] > total) continue;
+
+            sides[j] += nums[i];
+            // const uint64_t m = mask | (1 << ((j * 16) + i));
+            if (dfs(total, nums, i + 1, mask)) return true;
+            // dp.emplace(m);
+            sides[j] -= nums[i];
+
+            if (!sides[j]) break;
+        }
+
+        return false;
+    }
+
+    std::vector<int> sides;
+    std::unordered_set<uint64_t> dp;
+};
