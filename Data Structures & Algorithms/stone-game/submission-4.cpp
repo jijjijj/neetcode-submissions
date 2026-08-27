31e5e47a866beb22115e40ccd13b95c5d2b001e8
jijjijj <realjijjijj@gmail.com>
2026-08-27 21:01:28 +0300

    Add: stone-game - submission-4

---
+++ Data Structures & Algorithms/stone-game/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        const int n = piles.size();
+        const int sum = std::accumulate(
+            piles.begin(), piles.end(), 0);
+
+        std::vector<std::vector<int>> dp(n + 1,
+            std::vector<int>(n + 1));
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j >= 0; --j) {
+                // [j, i]
+                const bool alice = (i + j) & 1;
+                const int left = alice ? piles[j] : 0;
+                const int right = alice ? piles[i] : 0;
+
+                dp[j][i] = std::max(
+                    dp[j + 1][i] + left,
+                    (i ? dp[j][i - 1] : 0) + right);
+            }
+        }
+
+        return dp[0][n - 1] > (sum / 2);
+    }
+};
