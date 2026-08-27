6c388bbc5954dc81efe902c133d7ece5b58d245c
jijjijj <realjijjijj@gmail.com>
2026-08-28 00:21:44 +0300

    Add: stone-game-ii - submission-6

---
+++ Data Structures & Algorithms/stone-game-ii/submission-6.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        const int n = piles.size();
+
+        std::vector<std::vector<int>> dp(n + 1,
+            std::vector<int>(n + 1));
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int m = 1; m <= (n / 2 + 1); ++m) {
+                int total = 0;
+                for (int x = 1; x <= m * 2; ++x) {
+                    if (i + x > n) continue;
+
+                    total += piles[i + x - 1];
+
+                    dp[i][m] = std::max(dp[i][m],
+                        total - dp[i + x][std::max(x, m)]);
+                }
+            }
+        }
+
+        return (std::accumulate(piles.begin(),
+            piles.end(), 0) - dp[0][1]) / 2 + dp[0][1] / 2 * 2;
+    }
+};
