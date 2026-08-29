3fe2b401c4ed0d51aef6ae5f471754c1599b0768
jijjijj <realjijjijj@gmail.com>
2026-08-29 17:41:48 +0300

    Add: partition-to-k-equal-sum-subsets - submission-1

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        // std::ranges::sort(nums);
+
+        std::vector<int> subsets(k, 0);
+
+        return can(0, nums, k, subsets);
+    }
+
+private:
+    bool can(int i, const vector<int>& nums, int k,
+        std::vector<int>& subsets) {
+
+        if (i >= nums.size()) {
+            bool finished = true;
+            for (int j = 1; j < k; ++j) {
+                if (subsets[j] != subsets[j - 1]) {
+                    finished = false;
+                    break;
+                }
+            }
+
+            return finished;
+        }
+
+        for (int j = 0; j < k; ++j) {
+            subsets[j] += nums[i];
+            if (can(i + 1, nums, k, subsets)) return true;
+            subsets[j] -= nums[i];
+
+            if (!subsets[j]) break;
+        }
+
+        return false;
+    }
+};
