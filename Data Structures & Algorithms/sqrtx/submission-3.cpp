c656317b460a5f6018f5f54ef09b558ee997b7ff
jijjijj <realjijjijj@gmail.com>
2026-09-01 22:15:07 +0300

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2) return x;
+
+        int l = mySqrt(x / 4) * 2;
+        int r = l + 1;
+
+        return (long long)(r * r) <= x ? r : l;
+    }
+};
