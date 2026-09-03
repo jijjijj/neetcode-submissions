f68bc94352a886d403f7a063be4a4c8100a0eee7
jijjijj <realjijjijj@gmail.com>
2026-09-03 17:20:52 +0300

    Add: three-integer-sum - submission-13

---
+++ Data Structures & Algorithms/three-integer-sum/submission-13.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        std::ranges::sort(nums);
+
+        std::vector<std::vector<int>> res;
+
+        std::unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); ++i) mp[nums[i]] = i;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] > 0) break;
+            if (i && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                const long long val = -(nums[i] + nums[j]);
+                if (mp.contains(val) && mp[val] > j) {
+                    res.emplace_back(std::vector<int>{
+                        nums[i], nums[j], (int)val
+                    });
+                }
+            }
+        }
+
+        return res;
+    }
+};
