e788d4210a9ea3cc603437670a70f36c05d41cb7
jijjijj <realjijjijj@gmail.com>
2026-09-03 23:07:39 +0300

    Add: bitwise-and-of-numbers-range - submission-1

---
+++ Data Structures & Algorithms/bitwise-and-of-numbers-range/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // 2^(i+1)
+    // shift both until equal
+    // remove rightmost set bit until equal
+    int rangeBitwiseAnd(int left, int right) {
+        int res = 0;
+        for (int i = 0; i < 32; ++i) {
+            if (!(left & (1 << i))) continue;
+
+            int rem = (left % (1 << i));
+            if (left - rem + (1 << i) > right) {
+                res |= (1 << i);
+            }
+        }
+        return res;
+    }
+};
