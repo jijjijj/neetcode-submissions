d6a4509c5a0990e455e24de9efdb39833355d4e9
jijjijj <realjijjijj@gmail.com>
2026-09-05 12:28:58 +0300

    Add: design-hashmap - submission-2

---
+++ Data Structures & Algorithms/design-hashmap/submission-2.cpp
@@ -0,0 +1,79 @@
+class MyHashMap {
+    struct Node {
+        int key{};
+        int val{};
+        Node* next{};
+    };
+
+public:
+    MyHashMap() : buckets(1000) {
+        for (int i = 0; i < buckets.size(); ++i) {
+            buckets[i] = new Node{};
+        }
+    }
+    
+    void put(int key, int value) {
+        const int k = key % buckets.size();
+
+        Node* node = buckets[k];
+
+        while (node->next) {
+            if (node->next->key == key) {
+                node->next->val = value;
+                return;
+            }
+
+            node = node->next;
+        }
+
+        node->next = new Node{ .key = key, .val = value };
+    }
+    
+    int get(int key) {
+        const int k = key % buckets.size();
+        
+        Node* node = buckets[k];
+
+        while (node->next) {
+            // std::cout << node->next->key << " " << key << std::endl;
+
+            if (node->next->key == key) {
+                return node->next->val;
+            }
+
+            node = node->next;
+        }
+
+        return -1;
+    }
+    
+    void remove(int key) {
+        const int k = key % buckets.size();
+
+        Node* node = buckets[k];
+        
+        while (node->next) {
+            // std::cout << node->next->key << " " << key << std::endl;
+            if (node->next->key == key) {
+                Node* tmp = node->next;
+                node->next = node->next->next;
+                // std::cout << "new next " << node->next->key << std::endl;
+                delete tmp;
+                break;
+            }
+
+            node = node->next;
+        }
+    }
+
+private:
+    std::vector<Node*> buckets;
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
