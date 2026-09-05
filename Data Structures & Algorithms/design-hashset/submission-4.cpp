017bfbbf50705c3072e2483c49f16723d2b92e4a
jijjijj <realjijjijj@gmail.com>
2026-09-05 21:51:09 +0300

    Add: design-hashset - submission-4

---
+++ Data Structures & Algorithms/design-hashset/submission-4.cpp
@@ -0,0 +1,34 @@
+#include <cstring>
+
+class MyHashSet {
+public:
+    MyHashSet() {
+        memset(set, 0, sizeof(set) / sizeof(unsigned));
+    }
+    
+    void add(int key) {
+        set[key / 32] |= mask(key);
+    }
+    
+    void remove(int key) {
+        set[key / 32] &= ~mask(key);
+    }
+    
+    bool contains(int key) {
+        return set[key / 32] & mask(key);
+    }
+
+private:
+    unsigned int mask(unsigned int key) {
+        return (1u << (key % 32));
+    }
+    unsigned int set[100001 / 32 + 1]{};
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
