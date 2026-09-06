0e160170b67d1d2434f899325a91aa39b858f8c4
jijjijj <realjijjijj@gmail.com>
2026-09-07 00:41:53 +0300

    Add: integer-break - submission-3

---
+++ Data Structures & Algorithms/integer-break/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        std::vector<int> dp(n + 1, 1);
+
+        for (int i = 1; i <= n; ++i) {
+            dp[i] = (i == n) ? 0 : i;
+            for (int j = 1; j < i; ++j) {
+                dp[i] = std::max({ dp[i], j * dp[i - j] });
+            }
+        }
+
+        return dp.back();
+    }
+};
