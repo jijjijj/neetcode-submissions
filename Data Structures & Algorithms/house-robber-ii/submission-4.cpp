37e50f8853ea0d2f7dfca35ab907cf9482cabe5d
jijjijj <realjijjijj@gmail.com>
2026-09-11 21:25:51 +0300

    Add: house-robber-ii - submission-4

---
+++ Data Structures & Algorithms/house-robber-ii/submission-4.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        return std::max(dp(nums, 0), dp(nums, 1));
+    }
+
+private:
+    int dp(const vector<int>& nums, int i) {
+        std::vector<int> d(nums.size() + 2);
+        for (int j = nums.size() - i - 1; j >= 0; --j) {
+            d[j] = std::max(nums[j] + d[j + 2], d[j + 1]);
+        }
+        return d[1 - i];
+    }
+};
