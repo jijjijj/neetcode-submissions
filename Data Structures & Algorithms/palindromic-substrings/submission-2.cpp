37177235fbd44da7fa4fedbea343339d6f0902b3
jijjijj <realjijjijj@gmail.com>
2026-09-11 23:29:59 +0300

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int countSubstrings(string in) {
+        std::string s(in.size() * 2 + 1, '#');
+        for (int i = 0; i < in.size(); ++i) s[i * 2 + 1] = in[i];
+
+        std::vector<int> m(s.size(), 1);
+
+        int l = 0;
+        int r = -1;
+        int sum = 0;
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (i <= r) {
+                m[i] = std::min(l + (r - i), r - i + 1);
+            }
+
+            while (i + m[i] < s.size() &&
+                i - m[i] >= 0 &&
+                s[i + m[i]] == s[i - m[i]]) ++m[i];
+
+            if (i + m[i] - 1 > r) {
+                r = i + m[i] - 1;
+                l = i - m[i] + 1;
+            }
+
+            sum += (m[i] - (s[i] == '#')) / 2;
+        }
+
+        return sum;
+    }
+};
