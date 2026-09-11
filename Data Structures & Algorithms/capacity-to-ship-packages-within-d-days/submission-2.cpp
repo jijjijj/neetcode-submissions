364375d7484b1075ca2d72900e9d4afaf444e091
jijjijj <realjijjijj@gmail.com>
2026-09-11 19:56:54 +0300

    Add: capacity-to-ship-packages-within-d-days - submission-2

---
+++ Data Structures & Algorithms/capacity-to-ship-packages-within-d-days/submission-2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int l = *std::max_element(weights.begin(),
+            weights.end());
+        int r = 50000 * 500;
+
+        while (l <= r) {
+            const int mid = l + (r - l) / 2;
+
+            if (valid(weights, mid, days)) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return l;
+    }
+
+private:
+    bool valid(const std::vector<int>& weights,
+        int cap, int days) {
+        int cur = 0;
+        int used = 0;
+
+        for (const int w : weights) {
+            if (cur + w > cap) {
+                cur = 0;
+                ++used;
+            }
+            cur += w;
+        }
+
+        return used + 1 <= days;
+    }
+};
