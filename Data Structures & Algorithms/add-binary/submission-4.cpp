22d6f010e3904cd0063687f33f1e9590e1169c67
jijjijj <realjijjijj@gmail.com>
2026-09-14 17:43:19 +0300

    Add: add-binary - submission-4

---
+++ Data Structures & Algorithms/add-binary/submission-4.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        const int mx = std::max(a.size(), b.size());
+
+        std::ranges::reverse(a);
+        std::ranges::reverse(b);
+
+        std::string res;
+        res.resize(mx);
+
+        bool carry = 0;
+        for (int i = 0; i < mx; ++i) {
+            const bool v1 = i < a.size() ? a[i] - '0' : 0;
+            const bool v2 = i < b.size() ? b[i] - '0' : 0;
+
+            res[i] = (carry ^ v1 ^ v2) + '0';
+            carry = (v1 && v2) || (v1 && carry) || (v2 && carry);
+        }
+
+        // 10010
+        //   111
+        if (carry) res += '1';
+        
+        std::ranges::reverse(res);
+
+        return res;
+    }
+};
