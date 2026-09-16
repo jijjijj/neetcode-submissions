cc9e577d21fa78a69fc633163114b961a18398e4
jijjijj <realjijjijj@gmail.com>
2026-09-16 21:45:57 +0300

    Add: lemonade-change - submission-3

---
+++ Data Structures & Algorithms/lemonade-change/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int c5 = 0;
+        int c10 = 0;
+        int c20 = 0;
+
+        for (const int c : bills) {
+            switch (c) {
+                case 5:
+                    ++c5;
+                    break;
+                case 10:
+                    ++c10;
+                    --c5;
+                    break;
+                case 20: {
+                    if (c10) {
+                        --c10;
+                        --c5;
+                    } else {
+                        c5 -= 3;
+                    }
+                }
+            }
+
+            if (c5 < 0) return false;
+        }
+
+        return true;
+    }
+};
