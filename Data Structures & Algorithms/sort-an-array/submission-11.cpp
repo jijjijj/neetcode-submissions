40a6d229e8520cb79e0b646ae116d1cc0a59e2ab
jijjijj <realjijjijj@gmail.com>
2026-09-18 00:14:03 +0300

    Add: sort-an-array - submission-11

---
+++ Data Structures & Algorithms/sort-an-array/submission-11.cpp
@@ -0,0 +1,29 @@
+#include <ranges>
+
+class Solution {
+public:
+    // +quick sort - +iterative
+    // +merge sort - +iterative
+    // +heap sort - +iterative
+    // +radix sort
+    // +counting sort
+    // shell sort
+    // insertion sort
+    // selection sort
+    // bucket sort
+    vector<int> sortArray(vector<int>& nums) {
+        const int n = nums.size();
+        
+        for (int i = 0; i < n; ++i) {
+            int min = i;
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+            std::swap(nums[min], nums[i]);
+        }
+        
+        return nums;
+    }
+};
