237710cdefbcca520a20707ff31472db5fad4eb1
jijjijj <realjijjijj@gmail.com>
2026-09-19 23:37:30 +0300

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    // +sorting
+    // bit
+    // rand
+    // boyer moore
+    int majorityElement(vector<int>& nums) {
+        int res = 0;
+        for (int i = 0; i < 32; ++i) {
+            int cnt = 0;
+            for (const int val : nums) {
+                cnt += !!(val & (1 << i));
+            }
+
+            if (cnt > (nums.size() / 2)) {
+                res |= (1 << i);
+            }
+        }
+        return res;
+    }
+};
