330774740acbfe1209315b76382a2fd9673709b9
jijjijj <realjijjijj@gmail.com>
2026-09-20 14:02:40 +0300

    Add: remove-element - submission-5

---
+++ Data Structures & Algorithms/remove-element/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int last = 0;
+        int i = nums.size() - 1;
+        
+        // 1 2 3, val = 1
+        while (last <= i) {
+            if (nums[last] == val) {
+                nums[last] = nums[i--];
+            } else {
+                ++last;
+            }
+        }
+
+        return last;
+    }
+};
