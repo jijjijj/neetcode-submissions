5a0d0fee5a87f063107f38683e96d367c94281b5
jijjijj <realjijjijj@gmail.com>
2026-09-20 14:17:04 +0300

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    // rec
+    // stack
+    void reverseString(vector<char>& str) {
+        std::stack<char> s;
+
+        for (const char c : str) s.push(c);
+
+        int i = 0;
+        while (!s.empty()) {str[i++]
+    }
+};
