e240ce0c63a59a0ae5a827eaf239dcf5c9b19cc0
jijjijj <realjijjijj@gmail.com>
2026-09-20 13:11:39 +0300

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        std::array<int, 3> t = { 0, 1, 1 };
+
+        for (int i = 3; i <= n; ++i) {
+            t[i % 3] = t[0] + t[1] + t[2];
+        }
+
+        return t[n % 3];
+    }
+};
