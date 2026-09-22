ec6aa9e7f5a17c0bcc850f46b1d023c83ef575fc
jijjijj <realjijjijj@gmail.com>
2026-09-22 21:15:25 +0300

    Add: partition-to-k-equal-sum-subsets - submission-7

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-7.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // +backtracking + pruning + bitmask
+    // dp bitmask
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        const int total = std::accumulate(nums.begin(), nums.end(), 0);
+        if (total % k) return false;
+        const int target = total / k;
+
+        std::sort(nums.rbegin(), nums.rend());
+
+        const int n = nums.size();
+        const int max_mask = (1 << n);
+        std::vector<int> dp(max_mask, -1);
+        dp[0] = 0;
+
+        for (int i = 0; i < max_mask; ++i) {
+            if (dp[i] == -1) continue;
+            
+            for (int j = 0; j < n; ++j) {
+                if ((i & (1 << j)) || (dp[i] + nums[j] > target)) continue;
+
+                dp[i | (1 << j)] = (dp[i] + nums[j]) % target;
+            }
+        }
+
+        return !dp.back();
+    }
+};
