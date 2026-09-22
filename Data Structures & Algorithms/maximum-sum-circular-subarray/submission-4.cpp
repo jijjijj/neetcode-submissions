0bb5f9dbe627a7be8e8abd9feb444881421f47bb
jijjijj <realjijjijj@gmail.com>
2026-09-22 23:00:43 +0300

    Add: maximum-sum-circular-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-4.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        const int n = nums.size();
+
+        std::vector<int> suffixMax(n + 1);
+        suffixMax.back() = std::numeric_limits<int>::min();
+
+        const int total = std::accumulate(
+            nums.begin(), nums.end(), 0);
+
+        int current = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            current += nums[i];
+            suffixMax[i] = std::max(suffixMax[i + 1], current);
+        }
+
+        int prefix = 0;
+        current = 0;
+        int ans = 0; // !
+
+        for (int i = 0; i < n; ++i) {
+            current = std::max(current + nums[i], nums[i]);
+            ans = std::max(current, ans);
+
+            prefix += nums[i];
+            if (i + 1 < n) {
+                ans = std::max(ans, prefix + suffixMax[i + 1]);
+            }
+        }
+
+        return ans;
+    }
+};
