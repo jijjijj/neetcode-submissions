82cd494809603d676db10e9637f07262348a041f
jijjijj <realjijjijj@gmail.com>
2026-09-25 23:23:46 +0300

    Add: first-missing-positive - submission-0

---
+++ Data Structures & Algorithms/first-missing-positive/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        for (int i = 0; i < nums.size(); ) {
+            if (nums[i] <= 0) {
+                ++i;
+            } else if (nums[i] >= nums.size()) {
+                ++i;
+            } else if (nums[i] != i + 1 &&
+                nums[i] != nums[nums[i] - 1]) {
+                std::swap(nums[i], nums[nums[i] - 1]);
+            } else {
+                ++i;
+            }
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+
+        return -1;
+    }
+};
