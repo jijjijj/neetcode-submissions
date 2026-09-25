4ccdf6b703d19835681c181ec24f2d4cfba874d8
jijjijj <realjijjijj@gmail.com>
2026-09-25 20:36:56 +0300

    Add: subarray-sum-equals-k - submission-1

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        const int n = nums.size();
+
+        std::vector<int> suffix(nums.size() + 1);
+        std::unordered_map<int, int> mp2; // suffix, count
+
+        mp2[0] = 1;
+
+        for (int i = n - 1; i >= 0; --i) {
+            suffix[i] = suffix[i + 1] + nums[i];
+            ++mp2[suffix[i]];
+        }
+
+        int cnt = 0;
+
+        for (int i = 0; i < n; ++i) {
+            cnt += mp2[suffix[i] - k];
+        }
+
+        return cnt;
+    }
+};
